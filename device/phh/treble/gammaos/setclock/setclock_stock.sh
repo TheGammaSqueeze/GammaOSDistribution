5b647f88f30a9033c912e92f535bcccad6a19b81
TheGammaSqueeze <116582950+TheGammaSqueeze@users.noreply.github.com>
2023-12-18 20:36:21 +0000

    Customizations (#10)
    * Force 90 degrees landscape rotation for RG405M
    
    * Fixes for 405M display and cpu scaling
    
    * Revert "Force 90 degrees landscape rotation for RG405M"
    
    This reverts commit 4013e830b6193bc0044c7c23fc9bdecd9227fe63.

--- device/phh/treble/gammaos/setclock/setclock_stock.sh
+++ device/phh/treble/gammaos/setclock/setclock_stock.sh
@@ -1,15 +1,25 @@
 #!/system/bin/sh
 
-echo "simple_ondemand" > /sys/class/devfreq/fde60000.gpu/governor
-echo "200000000" > /sys/class/devfreq/fde60000.gpu/min_freq
-echo "800000000" > /sys/class/devfreq/fde60000.gpu/max_freq
-echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
-echo "408000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
-echo "1800000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
-echo "dmc_ondemand" > /sys/class/devfreq/dmc/governor
-echo "324000000" > /sys/class/devfreq/dmc/min_freq
-echo "1056000000" > /sys/class/devfreq/dmc/max_freq
+echo "simple_ondemand" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/governor
+echo "384000000" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/min_freq
+echo "850000000" > /sys/devices/platform/soc/soc:mm/60000000.gpu/devfreq/60000000.gpu/max_freq
 
-echo "0" > /sys/class/leds/battery_full/brightness
-echo "255" > /sys/class/leds/battery_charging/brightness
-echo "0" > /sys/class/leds/low_power/brightness
+echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy0/scaling_governor
+echo 0 > /sys/devices/system/cpu/cpufreq/policy0/scaling_fix_freq
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy0/scaling_min_freq
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy0/scaling_max_freq
+
+echo "schedutil" > /sys/devices/system/cpu/cpufreq/policy6/scaling_governor
+echo 0 > /sys/devices/system/cpu/cpufreq/policy6/scaling_fix_freq
+echo "614400" > /sys/devices/system/cpu/cpufreq/policy6/scaling_min_freq
+echo "2002000" > /sys/devices/system/cpu/cpufreq/policy6/scaling_max_freq
+
+echo 0 > /sys/class/devfreq/scene-frequency/sprd-governor/scaling_force_ddr_freq
+echo "simple_ondemand" > /sys/class/devfreq/scene-frequency/governor
+echo "50" > /sys/class/devfreq/scene-frequency/polling_interval
+echo "256" > /sys/class/devfreq/scene-frequency/min_freq
+echo "1866" > /sys/class/devfreq/scene-frequency/max_freq
+
+echo "255" > /sys/class/leds/sc27xx\:blue/brightness
+echo "0" > /sys/class/leds/sc27xx\:green/brightness
+echo "0" > /sys/class/leds/sc27xx\:red/brightness
